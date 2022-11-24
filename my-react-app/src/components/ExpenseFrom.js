61768ee8b716f0d8a245e0f5971324f566d9a944
Julia Katharina Nee <juliakatharina77@icloud.com>
2022-11-24 12:50:58 +0100

    Updateuuid

---
+++ my-react-app/src/components/ExpenseFrom.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseFrom = () => {
+  return <div>Hello from expenseFrom</div>;
+};
+
+export default ExpenseFrom;
---
+++ my-react-app/src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return <div>Hey from ExpenseItem</div>;
+};
+
+export default ExpenseItem;
